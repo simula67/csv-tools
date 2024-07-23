4ad81cab2b6a92c1651c4240b5f8ceef733aa86b
Joji Antony <simula67@gmail.com>
2024-07-23 11:31:01 +0530

    Change CSV processing to multiple CSVs and not only 2

--- mergecsv.py
+++ mergecsv.py
@@ -1,38 +1,46 @@
 import argparse
+import csv
 
 
-# Function to merge two CSV files without duplicating the headers
-def merge_csvs(input_csv1, input_csv2, output_csv):
+# Function to merge multiple CSV files without duplicating the headers
+def merge_csvs(input_csvs, output_csv):
 
-    # Check if two CSV files have the same headers
-    if not check_headers(input_csv1, input_csv2):
+    # Check if all CSV files have the same headers
+    if not check_headers(input_csvs):
         return 1, "Error: CSV files have different headers."
 
-    with open(input_csv1, 'r') as f1, open(input_csv2, 'r') as f2, open(output_csv, 'w') as out:
-        for line in f1:
-            out.write(line)
-        header = True
-        for line in f2:
-            if header:
-                header = False
-                continue
-            out.write(line)
+    with open(output_csv, 'w') as out:
+        header_written = False
+        for input_csv in input_csvs:
+            with open(input_csv, 'r') as f:
+                for line_number, line in enumerate(f):
+                    if line_number == 0 and header_written:
+                        continue
+                    if line_number == 0:
+                        header_written = True
+                    out.write(line)
     return 0, "CSV files merged successfully."
 
 
-# Check if two CSV files have the same headers
-def check_headers(input_csv1, input_csv2):
-    with open(input_csv1, 'r') as f1, open(input_csv2, 'r') as f2:
-        header1 = f1.readline().strip()
-        header2 = f2.readline().strip()
-    return header1 == header2
+# Check if all CSV files have the same headers
+def check_headers(input_csvs):
+    with open(input_csvs[0], 'r') as f:
+        reader = csv.reader(f)
+        reference_header = next(reader)
+
+    for input_csv in input_csvs[1:]:
+        with open(input_csv, 'r') as f:
+            reader = csv.reader(f)
+            header = next(reader)
+            if header != reference_header:
+                return False
+    return True
 
 
 if __name__ == '__main__':
-    # Accept command line arguments (using argparse) for two csvs and merge them
+    # Accept command line arguments (using argparse) for multiple CSVs and merge them
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_csv1', help='Input CSV file 1')
-    parser.add_argument('input_csv2', help='Input CSV file 2')
+    parser.add_argument('input_csvs', nargs='+', help='Input CSV files')
     parser.add_argument('output_csv', help='Output CSV file')
     args = parser.parse_args()
-    merge_csvs(args.input_csv1, args.input_csv2, args.output_csv)
+    merge_csvs(args.input_csvs, args.output_csv)
